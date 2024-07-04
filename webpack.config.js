11dc90dca79514b78a8bafb0645b9935297db15a
BentlyM <metayerbently@gmail.com>
2024-07-04 12:20:27 -0400

    this wasnt needed

--- webpack.config.js
+++ webpack.config.js
@@ -39,9 +39,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]',
-        },
       },
     ],
   },
