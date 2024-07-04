be29811051fbf6d14f8689449148c9a79b2c05fc
BentlyM <metayerbently@gmail.com>
2024-07-04 12:19:24 -0400

    added a config which allows me to muniplate images directly in html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -14,6 +15,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "ToDo's",
       template: './src/index.html',
+      publicPath: './',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/images', to: 'images' },
+      ],
     }),
   ],
   output: {
@@ -32,6 +39,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
       },
     ],
   },
