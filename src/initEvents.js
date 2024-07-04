6330aab4f9370eb40126ccc1afa3875e681231bf
BentlyM <metayerbently@gmail.com>
2024-07-04 18:42:34 -0400

    small notes

--- src/initEvents.js
+++ src/initEvents.js
@@ -1,9 +1,12 @@
 import Tabs from './tabs.js';
 
 export const initializeEvents = () => {
+
     document.querySelectorAll('.content-sect').forEach(item => {
         item.addEventListener('click', (event) => {
             console.log(event.target.textContent);
             Tabs().changeTitle(event.target.textContent);
     })
+
+
 })}
