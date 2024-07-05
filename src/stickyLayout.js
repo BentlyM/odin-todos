dfd3a4777c85582ec646420a5340e2a39d106e3d
BentlyM <metayerbently@gmail.com>
2024-07-05 10:42:32 -0400

    added some test card

--- src/stickyLayout.js
+++ src/stickyLayout.js
@@ -1,7 +1,13 @@
 export const StickyComponent = () => {
+    let stickyWallNotes = [];
+
     const stickyLayout = document.createElement('div');
     stickyLayout.classList.add('stickyContainer');
 
+    const testCard = document.createElement('div');
+    testCard.classList.add('Card');
+    stickyLayout.appendChild(testCard);
+    
     return {
         stickyLayout
     }
